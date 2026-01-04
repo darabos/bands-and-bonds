19c7706a3aeb68513b324d2d12c119c58eca4e78
Daniel Darabos <darabos.daniel@gmail.com>
2026-01-04 14:35:49 +0100

    Show/hide attack type tooltip with CSS instead of JS.

--- src/components/TagsWithTooltip.vue
+++ src/components/TagsWithTooltip.vue
@@ -1,44 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 const props = defineProps<{
   tags?: string[],
 }>();
-
-const textVisible = ref(false);
-
-function showText() {
-  textVisible.value = true;
-}
-
-function hideText() {
-  textVisible.value = false;
-}
 </script>
 
 <template>
-  <div class="tags" v-if="props.tags && props.tags.length > 0" @mouseenter="showText" @mouseleave="hideText">
-    <template v-if="textVisible">
-      <div class="tag-with-text" v-for="tag in props.tags">
-        <span class="tag" :class="tag" /> <span class="tag-text">{{ tag }}</span>
-      </div>
-    </template>
-    <div v-else>
-      <span class="tag" :class="tag" :key="tag" v-for="tag in props.tags" />
+  <div class="tags" v-if="props.tags && props.tags.length > 0">
+    <div class="tag-with-text" v-for="tag in props.tags">
+      <span class="tag" :class="tag" /> <span class="tag-text">{{ tag }}</span>
     </div>
   </div>
-
 </template>
 <style scoped>
-.tags {
+.tag-text {
+  display: none;
+}
+
+.tag-with-text {
+  display: inline-block;
+  white-space: nowrap;
+}
+
+.tags:hover {
   margin: auto;
   width: fit-content;
   font-size: 16px;
   display: flex;
   flex-direction: column;
   align-items: start;
-}
 
-.tag-with-text {
-  white-space: nowrap;
+  .tag-text {
+    display: inline;
+  }
 }
 </style>
